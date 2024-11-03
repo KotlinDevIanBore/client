f332fe3f7e3107e01af944db6ba252d28311c1e9
KotlinDevIanBore <iankipchirchirb3@gmail.com>
2024-11-03 23:12:23 +0300

    Removed headers from request

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -44,9 +44,7 @@ export async function createMissingPerson(formData: FormData) {
     try {
         const response = await fetch(url, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
+            
             body: formData,
         });
 
